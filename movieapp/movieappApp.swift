910a5b127b57987f4a3d588d9c0f1943d6d4133a
David <dmr617805@lasallebajio.edu.mx>
2024-12-01 00:45:43 +0100

    version incial

---
+++ movieapp/movieappApp.swift
@@ -0,0 +1,17 @@
+//
+//  movieappApp.swift
+//  movieapp
+//
+//  Created by m1 on 01/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct movieappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
